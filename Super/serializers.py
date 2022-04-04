6db3fbe07c9a198444899dd3661dd85de3c31dae
Austin <ofthevalley111@gmail.com>
2022-04-04 15:48:54 -0500

    created a basic GET serializer for all apps + created the URL file for both apps

--- Super/serializers.py
+++ Super/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Super, super_type
 
-model = Super
-fields =['name','alter_ego','primary_ability','secondary_ability','catchphrase','super_type','super_type_id']
-depth = 1
+class SuperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super
+        fields =['name','alter_ego','primary_ability','secondary_ability','catchphrase','super_type','super_type_id']
+        depth = 1
 
 
 
--- Super/views.py
+++ Super/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import super_type
+from .serializers import SuperSerializer
+from super_types import serializers
 
-# Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+@api_view(['GET'])
+def get_all_supers(request):
+   return Response('ok')
