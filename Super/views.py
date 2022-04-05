c39f2a7c46756f2adb9284a550a648cacb523a19
Austin <ofthevalley111@gmail.com>
2022-04-05 11:25:04 -0500

    added the API postman collection to gihub + created superuser on admin site + created values for the super_user and Super tables + created the views functions for the Super app + updated all urls for apps

--- Super/views.py
+++ Super/views.py
@@ -2,21 +2,45 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import super_type
+from .models import Super
 from .serializers import SuperSerializer
 from super_types import serializers
 
 
 
 
-
-
-
-
-
-
-
-
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def get_all_supers(request):
-   return Response('ok')
+   if request.method == 'GET':
+      super = Super.objects.all()
+      serializer = SuperSerializer(super, many=True)
+      return Response(serializer.data)
+
+   elif request.method == 'POST':
+       serializer = SuperSerializer(data=request.data)
+       serializer.is_valid(raise_exception=True)
+       serializer.save()
+       return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET','PUT','DELETE'])
+def super_detail(request,pk):
+   super = get_object_or_404(Super,pk=pk)
+   if request.method == 'GET':
+      serializer = SuperSerializer(super);
+      return Response(serializer.data)
+   elif request.method =='PUT':
+      serializer = SuperSerializer(super, data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data)
+   elif request.method == 'DELETE':
+      super.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
--- super_types/admin.py
+++ super_types/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import super_type
 
-# Register your models here.
+
+admin.site.register(super_type)
